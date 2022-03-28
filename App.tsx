40f1539faf491e7454a917b4fba89ab042a14167
Unkwin <102135066+Unkwin@users.noreply.github.com>
2022-03-28 22:08:24 +0800

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+
+import useCachedResources from './hooks/useCachedResources';
+import useColorScheme from './hooks/useColorScheme';
+import Navigation from './navigation';
+
+import 'react-native-gesture-handler';
+
+export default function App() {
+  const isLoadingComplete = useCachedResources();
+  const colorScheme = useColorScheme();
+
+  if (!isLoadingComplete) {
+    return null;
+  } else {
+    return (
+      <SafeAreaProvider>
+        <Navigation colorScheme={'light'} />
+        <StatusBar backgroundColor="black" />
+      </SafeAreaProvider>
+    );
+  }
+}
